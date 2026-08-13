2109d8b64ecfcd36604ce2033f32685f2d496a32
MD Mahade Hasan Talukder <mahade.prof.acc@gmail.com>
2026-08-13 13:46:08 -0700

    Add asset registry dependency for prototype visuals

--- Source/RoadsideIdiots/RoadsideIdiots.Build.cs
+++ Source/RoadsideIdiots/RoadsideIdiots.Build.cs
@@ -5,6 +5,15 @@ public class RoadsideIdiots : ModuleRules
     public RoadsideIdiots(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "AssetRegistry"
+        });
     }
 }
