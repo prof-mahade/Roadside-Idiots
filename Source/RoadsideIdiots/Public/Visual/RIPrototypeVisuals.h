b675a969ddd6f8b9731a5b0a65272031d78fadd5
MD Mahade Hasan Talukder <mahade.prof.acc@gmail.com>
2026-08-13 14:32:45 -0700

    Add prototype visual transform update hook

--- Source/RoadsideIdiots/Public/Visual/RIPrototypeVisuals.h
+++ Source/RoadsideIdiots/Public/Visual/RIPrototypeVisuals.h
@@ -5,6 +5,7 @@ class ARIBikePawn;
 namespace RIPrototypeVisuals
 {
     void Setup(ARIBikePawn* Bike);
+    void Update(ARIBikePawn* Bike);
     void PlaySideAction(ARIBikePawn* Bike, float Side);
     void PlayReaction(ARIBikePawn* Bike, float Side);
 }
