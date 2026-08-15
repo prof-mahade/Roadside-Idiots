066adb724cce202d856a0706ca15bae9d45fb697
MD Mahade Hasan Talukder <mahade.prof.acc@gmail.com>
2026-08-15 08:00:29 -0700

    VPR-23B add pause settings restart and quit menu state

--- Source/RoadsideIdiots/Public/Core/RIPlayerController.h
+++ Source/RoadsideIdiots/Public/Core/RIPlayerController.h
@@ -4,6 +4,14 @@
 #include "GameFramework/PlayerController.h"
 #include "RIPlayerController.generated.h"
 
+enum class ERIMenuMode : uint8
+{
+    None,
+    RaceSetup,
+    Pause,
+    Settings
+};
+
 UCLASS()
 class ROADSIDEIDIOTS_API ARIPlayerController : public APlayerController
 {
@@ -15,17 +23,31 @@ public:
     virtual void BeginPlay() override;
     virtual void SetupInputComponent() override;
 
-    bool IsRaceSetupMenuOpen() const { return bRaceSetupMenuOpen; }
-    int32 GetSelectedSetupRow() const { return SelectedSetupRow; }
+    bool IsRaceSetupMenuOpen() const { return MenuMode == ERIMenuMode::RaceSetup; }
+    bool IsPauseMenuOpen() const { return MenuMode == ERIMenuMode::Pause; }
+    bool IsSettingsMenuOpen() const { return MenuMode == ERIMenuMode::Settings; }
+    bool IsAnyMenuOpen() const { return MenuMode != ERIMenuMode::None; }
+    int32 GetSelectedMenuRow() const { return SelectedMenuRow; }
 
 private:
-    void SetupPrevious();
-    void SetupNext();
-    void SetupDecrease();
-    void SetupIncrease();
-    void SetupConfirm();
-    void AdjustSelectedSetting(int32 Delta);
+    void MenuPrevious();
+    void MenuNext();
+    void MenuDecrease();
+    void MenuIncrease();
+    void MenuConfirm();
+    void TogglePauseMenu();
 
-    bool bRaceSetupMenuOpen = true;
-    int32 SelectedSetupRow = 0;
+    void AdjustSelectedSetting(int32 Delta);
+    int32 GetCurrentMenuRowCount() const;
+    void OpenSettingsMenu();
+    void ReturnFromSettings();
+    void ResumeGame();
+    void RestartConfiguredRace();
+    void ReturnToRaceSetup();
+    void QuitToDesktop();
+    void ReloadCurrentLevel(bool bAutoStartConfiguredRace);
+
+    ERIMenuMode MenuMode = ERIMenuMode::RaceSetup;
+    ERIMenuMode ReturnMenuAfterSettings = ERIMenuMode::RaceSetup;
+    int32 SelectedMenuRow = 0;
 };
