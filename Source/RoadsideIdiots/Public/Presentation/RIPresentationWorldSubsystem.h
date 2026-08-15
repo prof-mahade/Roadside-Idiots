2ccc9190d92f64e4a35ff1f6cf5beb25af1b01e3
MD Mahade Hasan Talukder <mahade.prof.acc@gmail.com>
2026-08-15 04:45:58 -0700

    VPR-15 add presentation event world subsystem

---
+++ Source/RoadsideIdiots/Public/Presentation/RIPresentationWorldSubsystem.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RIPresentationWorldSubsystem.generated.h"
+
+class ARIBikePawn;
+class ARIRaceManager;
+
+UCLASS()
+class ROADSIDEIDIOTS_API URIPresentationWorldSubsystem : public UTickableWorldSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual TStatId GetStatId() const override;
+    virtual bool IsTickable() const override;
+
+private:
+    void UpdateRaceCues();
+    void UpdateCrashCues();
+    ARIBikePawn* FindHumanBike() const;
+
+    UPROPERTY()
+    TObjectPtr<ARIRaceManager> CachedRaceManager;
+
+    TMap<TWeakObjectPtr<ARIBikePawn>, bool> LastTippedState;
+    float CrashScanAccumulator = 0.0f;
+    int32 LastCountdownNumber = INDEX_NONE;
+    bool bPlayedGoCue = false;
+};
