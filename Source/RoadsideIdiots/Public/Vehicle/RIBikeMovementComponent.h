43b1da6be6a4dca84254ab5a0268ad9eee8e9901
MD Mahade Hasan Talukder <mahade.prof.acc@gmail.com>
2026-08-16 04:07:16 -0700

    Audio: keep bike movement physics-only

--- Source/RoadsideIdiots/Public/Vehicle/RIBikeMovementComponent.h
+++ Source/RoadsideIdiots/Public/Vehicle/RIBikeMovementComponent.h
@@ -46,7 +46,6 @@ private:
     void UpdateGroundedState();
     void ApplyDrive(float DeltaTime, UPrimitiveComponent* Body);
     void ApplySteeringAndBalance(float DeltaTime, UPrimitiveComponent* Body);
-    void UpdateDriveAudio(float DeltaTime, UPrimitiveComponent* Body);
 
     UPROPERTY(EditAnywhere, Category="Tuning|Speed")
     float MaxSpeedKph = 155.0f;
@@ -96,10 +95,6 @@ private:
 
     UPROPERTY(EditAnywhere, Category="Tuning|Handling")
     float GroundTraceLength = 145.0f;
-
-    // Player-only prototype audio cadence. These timers do not influence physics.
-    float EngineAudioAccumulator = 0.0f;
-    float SkidAudioCooldownRemaining = 0.0f;
 
     float ThrottleInput = 0.0f;
     float SteeringInput = 0.0f;
